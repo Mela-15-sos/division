bc1315b66f544c081e11d959a1a387c228a52bb1
Mela-15-sos <sossmela037@gmail.com>
2025-04-28 08:30:30 -0600

    Add files via upload

---
+++ division c++.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main(){
+int num1 , num2 , result;
+std::cout<<("programa para dividir dos nums\n");
+std::cout<<("ingresa el primer num\n");
+std::cin>>num1;
+std::cout<<("ingresa el segundo num\n");
+std::cin>> num2;
+result=num1/num2;
+std::cout<<("el resultado es:%d\n",result);
+return 0;
+}
